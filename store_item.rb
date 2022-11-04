a199f2d3b0835411e3330695c2e7d320d0eacef2
schristinelee <christinelee1527@gmail.com>
2022-11-03 23:40:10 -0700

    printed out the info in the hashes

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,7 @@
 fruit1 = { :name => "banana", :color => "yellow", :price => 3 }
 fruit2 = { name: "apple", color: "red", price: 2 }
 fruit3 = { :name => "plum", :color => "purple", :price => 4 }
+
+puts "A #{fruit1[:name]} is #{fruit1[:color]} and it's #{fruit1[:price]} dollars."
+puts "A #{fruit2[:name]} is #{fruit2[:color]} and it's #{fruit2[:price]} dollars."
+puts "A #{fruit3[:name]} is #{fruit3[:color]} and it's #{fruit3[:price]} dollars."
