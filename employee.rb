1dd97bf9641623ac7de93d7c1d2124a9d66c71a8
schristinelee <christinelee1527@gmail.com>
2022-11-06 21:25:45 -0800

    uses each loop

--- employee.rb
+++ employee.rb
@@ -75,13 +75,9 @@ class Manager < Employee
 
   def give_all_raises
     puts "gonna give everyone a raise!"
-    index = 0
-    @employees.length.times do
-      @employees[index].give_annual_raise
-      index = index + 1
+    @employees.each do |employee|
+      employee.give_annual_raise
     end
-    # employees[0].give_annual_raise
-    # employees[1].give_annual_raise
   end
 end
 
