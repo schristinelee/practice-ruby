dd641c2f2f1b3e4d99b4be03cc15abc8c052b794
schristinelee <christinelee1527@gmail.com>
2022-11-06 23:32:56 -0800

    namespaces

--- manager.rb
+++ manager.rb
@@ -1,25 +1,27 @@
 require "./employee.rb"
 require "./email_reportable.rb"
 
-class Manager < Employee
-  include EmailReportable
-
-  def initialize(input_options)
-    super
-    @employees = input_options[:employees]
-  end
-
-  def give_all_raises
-    puts "gonna give everyone a raise!"
-    @employees.each do |employee|
-      employee.give_annual_raise
+module Actualize
+  class Manager < Employee
+    include EmailReportable
+  
+    def initialize(input_options)
+      super
+      @employees = input_options[:employees]
     end
-  end
-
-  def fire_all_employees
-    puts "ran out of money! sorry :("
-    @employees.each do |employee|
-      employee.active = false
+  
+    def give_all_raises
+      puts "gonna give everyone a raise!"
+      @employees.each do |employee|
+        employee.give_annual_raise
+      end
+    end
+  
+    def fire_all_employees
+      puts "ran out of money! sorry :("
+      @employees.each do |employee|
+        employee.active = false
+      end
     end
   end
 end
